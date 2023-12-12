97066bdfb05e1376a0c58c6dab00f92400f06bb4
abdulhafiz <muhammedabdulhafiz1993@gmail.com>
2023-12-11 23:29:10 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Blog writer website by flask
---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+posts = [
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post content',
+        'content': 'Second post content',
+        'data_posted': 'April 21, 2018'
+    },
+    
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post content',
+        'content': 'Second post content',
+        'data_posted': 'April 21, 2018'
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("index.html", posts = posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
